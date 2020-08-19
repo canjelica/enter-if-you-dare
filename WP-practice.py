930cad41f6bfc6d64915ef43f5beea951efeeea8
Cassandra Sutton <cassandrasutton@gmail.com>
2020-08-18 21:27:00 -0600

    Added file

---
+++ WP-practice.py
@@ -0,0 +1,30 @@
+"""Return the date on second week of October in a Gregorian calendar"""
+
+
+"""Calculating whether a value is greater than the average value of consecutive subsets of data of certain window size."""
+
+find the average of subsets of window size, if value is greater return whatever
+
+Given an array and an integer K, find the maximum for each and every contiguous subarray of size k.
+Examples :
+
+Input: arr[] = {1, 2, 3, 1, 4, 5, 2, 3, 6}, K = 3 
+Output: 3 3 4 5 5 5 6
+Explanation: 
+Maximum of 1, 2, 3 is 3
+Maximum of 2, 3, 1 is 3
+Maximum of 3, 1, 4 is 4
+Maximum of 1, 4, 5 is 5
+Maximum of 4, 5, 2 is 5 
+Maximum of 5, 2, 3 is 5
+Maximum of 2, 3, 6 is 6
+
+Input: arr[] = {8, 5, 10, 7, 9, 4, 15, 12, 90, 13}, K = 4 
+Output: 10 10 10 15 15 90 90
+Explanation:
+Maximum of first 4 elements is 10, similarly for next 4 
+elements (i.e from index 1 to 4) is 10, So the sequence 
+generated is 10 10 10 15 15 90 90
+
+
+https://www.geeksforgeeks.org/sliding-window-maximum-maximum-of-all-subarrays-of-size-k/
