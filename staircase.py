ba293b863c0c4bb30de417af0a5c586260cfca13
Cassandra Sutton <cassandrasutton@gmail.com>
2020-08-31 21:58:20 -0600

    Added files

---
+++ staircase.py
@@ -0,0 +1,15 @@
+def staircase(n):
+    
+    n=4
+    3 spaces 1 #
+    2 spaces 2 #
+    1 space 3 #
+    0 spaces 4 #
+
+    tracker = n
+    while 
+    #need a loop to print out n lines
+    #for every line, n-1 spaces and then n - (n-1)
+    #next line n-2 spaces, then n - (n-2)
+    #n-3, n - (n-3)
+    # n#
