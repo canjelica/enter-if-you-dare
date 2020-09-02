dd2ded5c497860e83bfed41cc5a72dcf6bedee73
Cassandra Sutton <cassandrasutton@gmail.com>
2020-09-01 21:44:41 -0600

    Completed staircase

--- staircase.py
+++ staircase.py
@@ -1,13 +1,24 @@
 def staircase(n):
-    
-    n=4
-    3 spaces 1 #
-    2 spaces 2 #
-    1 space 3 #
-    0 spaces 4 #
+	i = 1
+	while i != n:
+		level = n-i
+		print((" " * level) + ("#" * (n-level)))
+		i += 1
 
-    tracker = n
-    while 
+
+
+   
+    # n=4
+    # 3 spaces 1 #
+    # 2 spaces 2 #
+    # 1 space 3 #
+    # 0 spaces 4 #
+
+    #need a loop to print out n lines
+    #for every line, n-1 spaces and then n - (n-1)
+    #next line n-2 spaces, then n - (n-2)
+    #n-3, n - (n-3)
+    # n#
     #need a loop to print out n lines
     #for every line, n-1 spaces and then n - (n-1)
     #next line n-2 spaces, then n - (n-2)
