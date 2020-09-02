8b90d2e35c92e0260ff320068f2d9dbac8200ba7
Cassandra Sutton <cassandrasutton@gmail.com>
2020-09-01 21:49:06 -0600

    added problem text

--- mini-max-sum.py
+++ mini-max-sum.py
@@ -1,1 +1,23 @@
 """
+
+Output Format
+
+Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. (The output can be greater than a 32 bit integer.)
+
+Sample Input
+
+1 2 3 4 5
+Sample Output
+
+10 14
+Explanation
+
+The numbers are , , , , and . Calculate the following sums using four of the five integers:
+
+Sum everything except , the sum is .
+Sum everything except , the sum is .
+Sum everything except , the sum is .
+Sum everything except , the sum is .
+Sum everything except , the sum is .
+Hints: Beware of integer overflow! Use 64-bit Integer.
+"""
