a8f108ef8ec1e0f8fbd8187ca1bc69ed4b753d21
Cassandra Sutton <cassandrasutton@gmail.com>
2020-09-02 22:23:56 -0600

    added whiteboarding ex

---
+++ waymo-ex.py
@@ -0,0 +1,105 @@
+# Write a function that takes in a list/array of maintenance events in chronological order and returns true if all maintenance was processed correctly. Otherwise, return false.
+
+# A MaintenanceEvent is a snapshot of maintenance performed on a self-driving car. When maintenance is started or completed, we capture that in a MaintenanceEvent.
+
+# A MaintenanceEvent can be open, signaling the maintenance is actively ongoing, or can be closed, signaling the maintenance has been completed.
+# A car can have multiple, unique, open maintenance.
+# If maintenance was properly processed, then:
+
+# MaintenanceEvents were opened/closed in the correct sequence.
+# A maintenance was closed after it was opened.
+# Any maintenance that was opened at some point in the future was closed.
+# There was no duplicate open maintenance of the same type for a car.
+
+
+class MaintenanceEvent(object):
+    def __init__(self, state, car_id, work_type, timestamp):
+        self.state = state # 1 means open, 0 means close
+        self.car_id = car_id
+        self.work_type = work_type
+        self.timestamp = timestamp
+
+def IsProcessedCorrectly(events):
+    #events[0].state
+    #events[0].car_id
+    #events[0].work_type
+    # TODO: implement properly!
+    return True
+
+
+assert(IsProcessedCorrectly(
+    [MaintenanceEvent(1, 809, "oil",  1000),
+     MaintenanceEvent(1, 600, "tires",1000),
+     MaintenanceEvent(1, 809, "filter", 1015),
+     MaintenanceEvent(0, 809, "oil",  1030),
+     MaintenanceEvent(0, 809, "filter", 1045),
+     MaintenanceEvent(0, 600, "tires",1200)]))
+
+
+#
+#I: array of 4 values 
+#loop over each item in events
+
+#{809: [MaintenanceEvent(1, 809, "oil",  1000), MaintenanceEvent(1, 809, "filter", 1015), MaintenanceEvent(0, 809, "oil",  1030), MaintenanceEvent(0, 809, "filter", 1045)]
+
+#tracker = {}
+
+#for event in events:
+    # if event.car_id in tracker:
+    #     open_events = tracker[event.car_id] #list of maintenanceevs
+    #     removed_event = False
+    #     for open_event in open_events:
+                
+    #         if event.work_type == open_event.work_type and                            event.state != open_event.state:
+    #            if open_event.timestamp < event.timestamp:
+   #                 open_events.remove(open_event)
+   #                 removed_event = True
+   #      if not removed_event:
+   #          if event.state == 1:
+   #                 tracker[event.car_id].append(event)
+#              else:
+                # return False
+#check if the dictionary has all empty values, if yes, True, if no, False
+            
+            
+
+                 
+    # else:
+        # tracker[event.car_id] = [event]
+    
+        
+
+#checking if event.car_id is a key in tracker, save event at event.car_id
+
+#check if work type is already in key, check that state is different, if state is 1, save timestamp_opn, state is 0, save timestamp_cls, if timestamp_opn < timestamp_cls, remove events
+#check dictionary for values at each existing key, as long as they're all empty, return true, return false
+
+# for each event
+#   # see if we have events for the car
+    # if yes
+       # for each open event
+          # do more stuff
+
+
+
+
+
+
+assert(not IsProcessedCorrectly(
+    [MaintenanceEvent(1, 81, "brakes",1000), # not closed
+     MaintenanceEvent(1, 82, "fuel",  1000),
+     MaintenanceEvent(0, 82, "fuel",  1030),
+     MaintenanceEvent(0, 82, "oil",   1200)])) # not opened
+
+# assert(!ProcessedCorrectly({{1, 11, "wash",  1000},
+#                               {1, 12, "filter",1000},
+#                               {0, 12, "filter",1030},
+#                               {1, 12, "oil",   1200}, # not closed
+#                               {0, 11, "wash",  1200},
+#                               {1, 13, "tires", 1200}})); # not closed
+
+# assert(!ProcessedCorrectly({{1, 8099, "wash", 800},
+#                               {1, 6780, "fuel", 830},
+#                               {1, 8099, "wash", 840}, # duplicate
+#                               {0, 6780, "fuel", 845},
+#                               {0, 8099, "wash", 1100}}));
