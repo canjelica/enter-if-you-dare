f8a2955c1ce83de24377d21dfbad6bb369069a27
Cassandra Sutton <cassandrasutton@gmail.com>
2020-09-19 21:14:15 -0600

    Added in line comments for debugging points and working on run class

--- maze_dist/src/python/mazes/MazeLoader.py
+++ maze_dist/src/python/mazes/MazeLoader.py
@@ -19,7 +19,8 @@ class ReferenceMazeRunner:
             exits = tuple(exits) #this is to be able to index into. Is there an advantage to being ordered?
         
         return exits
-    def get_rooms()
+
+    def get_rooms():
         """Returns an array of maze rooms."""
 
         rooms = list(self.maze.keys())
@@ -28,15 +29,29 @@ class ReferenceMazeRunner:
 
         return rooms
 
-
     def run(self, start, end):
 
-        maze_path = []
-    
+        # maze_path = []
 
-        return ['West', 'North', 'East', 'East'] #hard coded solution, so this is where I need to write a functino that explores the maze, and returns whatever values the path is. So, start is the dict value i begin with, then chain all the way, checking direction value to see if it matches the end. Once it matches the end, I return that array of saved steps in teh chain
+        # rooms = get_rooms()
+        # directions = get_directions()
+
+        # maze = self.maze
+
+        # if not start in maze:
+        #     return 
+
+
+
+
+        return ['North', 'East'] 
+        # start is the dict value i begin with, then chain all the way, checking direction value to see if it matches the end. Once it matches the end, I return that array of saved steps in teh chain
+
         #Make a list of all the possible directions
         #make a list of all the rooms
+        # initialize an empty maze path
+        #iterate over list of rooms and list of directions. if maze[room][exits][direction] has a value, append it to list.
+
 
 class MazeLoader:
     """Implements a maze and maze solver."""
@@ -45,8 +60,6 @@ class MazeLoader:
         self.master_list = {}
         
         try:
-            #for file in args as f
-            #wrapping this in for loop, to the length of args being passed in
             with open(sys.argv[1], 'r') as f:
                 cell_nums = int(f.readline())
                 for _ in range(cell_nums):
@@ -65,21 +78,21 @@ class MazeLoader:
                 start, end = f.readline().split(' ')
                 #maybe a bug here with how it's saving start, end. End not getting a value? Debug.
                 
-                
-                current = self.master_list.get(start)
+                current = self.master_list.get(start) #Room object
                 #Need to check to make sure the self.master.list[start] is a class object and not just the string "Entrance", same for "Kitchen" and end
                 runners = [ReferenceMazeRunner(self.master_list)]
                 #Figure out what the def init needs to have, and if it nees to be an array
                 for runner in runners:
                     result = runner.run(self.master_list.get(start), self.master_list.get(end))
                     #check to see if end/start are class obejcts, not strings
-                    for step in result:
-                        current = current.get_square(step)
+                    for step in result: #North, East
+                        current = current.get_square(step) #throws in run returned first direction, and returns the room at the direction, or None if it doesn't exist
                         #same check here for class instance
                         if current == None:
                             print('Invalid path returned')
                             break
-                    print('Returned ' + ('valid' if self.master_list.get(end).get_name() == current.get_name() else 'invalid') + ' path')
+                    if current == end: #There is a bug here, somehow getting None from get_square function
+                        print('Returned ' + ('valid' if self.master_list.get(end).get_name() == current.get_name() else 'invalid') + ' path')
         except FileNotFoundError:
             print('Location of maze file was not found')
         except IOError:
@@ -104,4 +117,4 @@ class MazeSquare:
     def get_square(self, direction):
         return self.exits.get(direction, None)                        
 
-# MazeLoader()
+MazeLoader()
