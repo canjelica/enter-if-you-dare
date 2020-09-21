5b103e5e76d46fcfbaaac4c3748cf83dc80aeceb
Cassandra Sutton <cassandrasutton@gmail.com>
2020-09-20 19:02:41 -0600

    Cleaned up MazeLoader to return valid maze dictionary using pdb

--- maze_dist/src/python/mazes/MazeLoader.py
+++ maze_dist/src/python/mazes/MazeLoader.py
@@ -1,12 +1,14 @@
 import sys
 from collections import deque
 
+
 # Mock submission that will return valid path
 class ReferenceMazeRunner:
     """Returns a valid path through a maze."""
     
     def __init__(self, maze):
-        self.maze = maze  #This has the start end keys
+        self.maze = maze         #this is the self.master_list created in Mazeloader and passed in as parameter
+
     
     def get_directions():
         """Returns all possible maze directions."""
@@ -32,25 +34,27 @@ class ReferenceMazeRunner:
 
     def run(self, start, end):
         """Returns a valid path through a maze using breadth-first search."""
-
-        maze = self.maze #this is the self.master_list created in Mazeloader and passed in as parameter
-        maze_rooms = deque([("", start)]) 
+        
+        maze_rooms = deque([("", start)])
         explored = set() 
+        
         
         while maze_rooms:
             path, current_room = maze_rooms.popleft()
-       
-        
-            possible_exits = maze[current_room].get('exits'))
-
-            if current_room = end:
+            current_room = current_room.name
+            possible_exits = self.maze[current_room].exits
+            
+            if current_room == end:
                 return path
 
             for direction, next_room in possible_exits.items():
+                breakpoint()
                 if not next_room in explored:
-                    maze_rooms.extend([path, direction], next_room)) 
- 
+                    maze_rooms.append(([path, direction], next_room))
+            
             explored.add(current_room)
+            
+        
 
 
 class MazeLoader:
@@ -59,62 +63,59 @@ class MazeLoader:
     def __init__(self):
         self.master_list = {}
 
-        def read_maze_file()
-            """Returns a dictionary of maze rooms."""
-            
-            try:
-                with open(sys.argv[1], 'r') as f:
-                    cell_nums = int(f.readline())
+        try:
+            with open(sys.argv[1], 'r') as f:
 
-                    for _ in range(cell_nums):
-                        curr_line = f.readline()
-                        parts = curr_line.split(' ', 2)
-                        name = parts[0]
+                # Begin reading file line-by-line
+                cell_nums = int(f.readline())
 
-                        if name not in self.master_list:
-                            self.master_list[name] = MazeSquare(name)
-                        
-                        square = self.master_list.get(name)
-                        exits = parts[1].split(',')
+                # Variableizes elements of each line
+                for _ in range(cell_nums):
+                    curr_line = f.readline()
+                    parts = curr_line.split(' ', 2)
+                    name = parts[0]
+                    # Adding MazeSquare object to dictionary by maze room name
+                    if name not in self.master_list:
+                        self.master_list[name] = MazeSquare(name)
+                    
+                    # Create variables for 
+                    square = self.master_list.get(name)
+                    doors = parts[1].split(',') #list of direction:room elements
+                    doors[-1] = doors[-1][:-1]
+                    
+                    for door in doors: 
+                        direction, next_square = door.split(':')
 
-                        for exit in exits: #is exit a reserved keyword?
-                            direction, next_square = exit.split(':')
+                        if next_square not in self.master_list:
+                            self.master_list[next_square] = MazeSquare(next_square) #change to str not object
 
-                            if next_square not in self.master_list:
-                                self.master_list[next_square] = MazeSquare(next_square)
-                            square.add_exit(self.master_list.get(next_square), direction)
+                        square.add_exit(next_square, direction)
+                
 
-                    start, end = f.readline().split(' ')
-                    #maybe a bug here with how it's saving start, end. End not getting a value? Debug.
+                #Adding start, end variables as strings
+                start, end = f.readline().split(' ')
+                breakpoint()
+            current_room = self.master_list.get(start) # This is a MazeSquare object
+            runners = [ReferenceMazeRunner(self.master_list)] 
 
-            except FileNotFoundError:
-                print('Location of maze file was not found')
-            except IOError:
-                print('IO Exception reading from maze file')
-            
-            def check_maze_run():
-                """Checks to see if maze run was a valid path."""
+            for runner in runners:
+                # This returns an array with a valid maze path through given maze
+                result = runner.run(self.master_list.get(start), self.master_list.get(end))
 
-                current = self.master_list.get(start) #Room object
-                    #Need to check to make sure the self.master.list[start] is a class object and not just the string "Entrance", same for "Kitchen" and end
-                    runners = [ReferenceMazeRunner(self.master_list)]
-                    #Figure out what the def init needs to have, and if it nees to be an array
+                for step in result:
+                    current_room = current_room.get_square(step) 
 
-                    for runner in runners:
-                        result = runner.run(self.master_list.get(start), self.master_list.get(end))
-                        #check to see if end/start are class obejcts, not strings
+                    if current_room == None:
+                        print('Invalid path returned')
+                        break
 
-                        for step in result: #North, East
-                            current = current.get_square(step) #throws in run returned first direction, and returns the room at the direction, or None if it doesn't exist
-                            #same check here for class instance
+                if current_room == end: 
+                    print('Returned ' + ('valid' if self.master_list.get(end).get_name() == current_room.get_name() else 'invalid') + ' path')
 
-                            if current == None:
-                                print('Invalid path returned')
-                                break
-
-                        if current == end: #There is a bug here, somehow getting None from get_square function
-                            print('Returned ' + ('valid' if self.master_list.get(end).get_name() == current.get_name() else 'invalid') + ' path')
-
+        except FileNotFoundError:
+            print('Location of maze file was not found')
+        except IOError:
+            print('IO Exception reading from maze file')
 
 class MazeSquare:
     """Implements a room in a maze."""
@@ -122,6 +123,9 @@ class MazeSquare:
     def __init__(self, name):
         self.name = name
         self.exits = {}
+    
+    def __repr__(self):
+        return f"< {self.name} >" 
         
     def add_exit(self, square, direction):                            
         self.exits[direction] = square
@@ -135,7 +139,7 @@ class MazeSquare:
     def get_square(self, direction):
         return self.exits.get(direction, None)                        
 
-# MazeLoader()
+MazeLoader()
 
 
 
