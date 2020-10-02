89e27eca92174822c887ac774ebe07f8a391d97b
Cassandra Sutton <cassandrasutton@gmail.com>
2020-10-01 21:39:18 -0600

    added to file

--- VMWare.py
+++ VMWare.py
@@ -25,3 +25,50 @@ Given two strings, find the longest common Substring
  resume experience in depth. Asked me to explain my project in depth. Questions were from leetcode medium level. There were 4 rounds. 2 of them were coding and one was project design explanation. Over a good experience
 
  
+
+ #!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+# Complete the printLinkedList function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def printLinkedList(head):
+    current = head
+    while current is not None:
+        print(current.data)
+        current = current.next
+
+if __name__ == '__main__':
